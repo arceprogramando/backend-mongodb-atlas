bc833d82806a1b534411167cc063f28e9188d594
ArceProg <arceprogramando@gmail.com>
2023-06-26 02:44:37 -0300

    agregado extensiones en eslint y middlewars

--- src/app.js
+++ src/app.js
@@ -1,12 +1,20 @@
 import express from 'express';
+import cors from 'cors';
+import __dirname from './utils.js';
 
 const app = express();
+
+app.use(cors());
+
+app.use(express.static(`${__dirname}/public`));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.set('port', 8080);
 
 app.listen(app.get('port'), () => {
   // eslint-disable-next-line no-console
-  console.log(`Encendido servidor en puerto ${app.get('port')} \n http://localhost:${app.get('port')}`);
+  console.log(`Encendido servidor en puerto ${app.get('port')} \n http://localhost:${app.get('port')}/`);
 });
 
 app.get('/', (req, res) => {

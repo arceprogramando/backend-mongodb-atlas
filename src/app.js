99a79fe18853b8424e668c31fcde10e97f03778b
ArceProg <arceprogramando@gmail.com>
2023-06-25 22:54:50 -0300

    agregado nodemon como parametro de desarrollo

--- src/app.js
+++ src/app.js
@@ -6,5 +6,9 @@ app.set('port', 8080);
 
 app.listen(app.get('port'), () => {
   // eslint-disable-next-line no-console
-  console.log(`Encendido servidor en puerto ${app.get('port')}`);
+  console.log(`Encendido servidor en puerto ${app.get('port')} \n http://localhost:${app.get('port')}`);
+});
+
+app.get('/', (req, res) => {
+  res.send('hola');
 });
